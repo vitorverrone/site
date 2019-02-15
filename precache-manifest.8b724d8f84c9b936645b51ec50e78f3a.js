1a9e948cc4037d4855555dc8428c2de193e20e3f
Vitor Verrone <vitorverrone@outlook.com>
2019-02-15 16:37:07 -0200

    Updates

---
+++ precache-manifest.8b724d8f84c9b936645b51ec50e78f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "10723f86e39b17406edd",
+    "url": "/site/static/js/main.10723f86.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/site/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "10723f86e39b17406edd",
+    "url": "/site/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "7d12822d60f352ef44e95996c1ea4464",
+    "url": "/site/index.html"
+  }
+];
